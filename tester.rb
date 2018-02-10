07624126b520beee71ca250e5610f0c05928d596
Corlin Reed <corlinreed@gmail.com>
2018-02-10 07:12:52 -0700

    fixed Tester script

--- tester.rb
+++ tester.rb
@@ -1,3 +1,4 @@
 puts "What would you like your US Senator from the Great State of Montana to do?"
-verb = gets.to_s
-puts "Please give a warm welcome to US Senator John #{verb}er"
+verb = gets.to_s.chomp!
+last_name = verb.capitalize + 'er'
+puts "Please give a warm welcome to US Senator John #{last_name}"
